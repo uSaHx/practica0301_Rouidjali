272f6c254b41aea3619a2399d92061279180c419
uSaHx <orouidj@educacion.navarra.es>
2023-01-16 13:43:43 +0100

    16/01.2

--- ejercicio1.py
+++ ejercicio1.py
@@ -4,17 +4,26 @@ def fibonacci_recusivo(n):
         return n
     else:
         return fibonacci_recusivo(n - 1) + fibonacci_recusivo(n - 2)
-def fibonacci_bulces(n):
-    f = 0
-    for x in range(n):
-        f += (x-1)+(x-2)
-    return f
+def fibonacci_bulces(numero):
+    n1, n2 = 0, 1
+    count = 0
+    if numero <= 0:
+       print("Error")
+    elif numero == 1:
+       print(n1)
+    else:
+       while count < numero:
+           nth = n1 + n2
+           n1 = n2
+           n2 = nth
+           count += 1
+    return
 inicio_r = datetime.datetime.now()
-print(fibonacci_recusivo(40))
+fibonacci_recusivo(40)
 fin_r = datetime.datetime.now()
-print("El tiempo de ejecución de la funcion recursiva es:", fin_r - inicio_r)
+print("El tiempo de ejecución del bucle:", fin_r - inicio_r)
 inicio_b = datetime.datetime.now()
-print(fibonacci_recusivo(40))
+fibonacci_bulces(40)
 fin_b = datetime.datetime.now()
 print("El tiempo de ejecución del bucle:", fin_b - inicio_b)
 
